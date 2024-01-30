5d5283e72b93999e4c8491278468e78fc8ef82f9
Zachary Trouy <34666901+ztrouy@users.noreply.github.com>
2024-01-30 16:45:58 -0600

    Alphabetized displayed items from cargoShip.js

--- scripts/cargoShip.js
+++ scripts/cargoShip.js
@@ -38,6 +38,22 @@ document.addEventListener(
 export const cargoShipsList = () => {
     const cargoShips = structuredClone(getCargoShips())
 
+    // Alphabetizes the Cargo Ships
+    cargoShips.sort((a, b) => {
+        // Ensures that case does not effect sorting
+        const nameA = a.name.toLowerCase()
+        const nameB = b.name.toLowerCase()
+        
+        // Informs Array Element which direction to move, if at all
+        if (nameA < nameB) {
+            return -1
+        } else if (nameA > nameB) {
+            return 1
+        } else {
+            return 0
+        }
+    })
+
     let cargoHTML = `<div class="header">Shipping Ships</div><ul>`
 
     for (const ship of cargoShips) {
